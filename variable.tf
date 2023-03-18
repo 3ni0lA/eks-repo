77de4044330f325ac960f4b7aad7d2aead7d9d4d
Adewale Eniola <104868415+3ni0lA@users.noreply.github.com>
2023-03-18 17:56:55 +0000

    initial commit

--- variable.tf
+++ variable.tf
@@ -7,8 +7,8 @@ variable "cluster_name" {
   default = "first-eks"
 }
 variable "aws_access_key" {
- default = "AKIA4BD7GPDOBG5Y5EPA" 
+ default = "" 
 }
 variable "aws_secret_key" {
-  default ="nf+dSWTg9IRM3fo3Dw3rgbaVKfHS4D5kHHsZqm3J"
+  default =""
 }
